9a7a98f987e286ee957966bec17ee87d64ff2ea6
bersavesab-hue <bersavesab@gmail.com>
2026-09-22 19:53:14 +0800

    chore: bump store skeleton release to v1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.gameuistudio.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "1.0.0"
+        versionCode = 14
+        versionName = "1.0.1"
     }
 
     buildTypes {
