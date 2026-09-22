bb0a25d6d0e7045712eea998fcea9072c63e2a22
bersavesab-hue <bersavesab@gmail.com>
2026-09-22 15:21:42 +0800

    chore: bump adaptation release to v0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.gameuistudio.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.4.2"
+        versionCode = 7
+        versionName = "0.5.0"
     }
 
     buildTypes {
