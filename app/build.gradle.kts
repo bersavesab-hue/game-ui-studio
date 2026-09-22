c71349fbb6cfc8ee9ab1142920a2180cb5b87432
bersavesab-hue <bersavesab@gmail.com>
2026-09-22 15:57:02 +0800

    chore: bump layout efficiency release to v0.8.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.gameuistudio.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 9
-        versionName = "0.7.0"
+        versionCode = 10
+        versionName = "0.8.0"
     }
 
     buildTypes {
