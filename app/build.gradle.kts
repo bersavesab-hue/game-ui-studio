82e99bd3049fcb9dfde03ac9b91b7632d0502ee1
bersavesab-hue <bersavesab@gmail.com>
2026-09-22 17:42:05 +0800

    chore: mark complete editor workflow as v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.gameuistudio.mobile"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.10.0"
+        versionCode = 13
+        versionName = "1.0.0"
     }
 
     buildTypes {
